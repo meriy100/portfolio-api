273f09c68c37ce6d59ce692e322b65c0585edbd0
meriy100 <ttattataa@gmail.com>
2022-11-13 14:52:10 +0900

    [Modify] histories の json key を camel case

--- entities/history.go
+++ entities/history.go
@@ -1,19 +1,19 @@
 package entities
 
 type History struct {
-	Organization string
-	Products     []Product
+	Organization string    `json:"organization"`
+	Products     []Product `json:"products"`
 }
 
 type Month struct {
-	Year  int
-	Month int
+	Year  int `json:"year"`
+	Month int `json:"month"`
 }
 
 type Product struct {
-	Title        string
-	StartMonth   Month
-	EndMonth     *Month
-	Description  []string
-	Technologies []string
+	Title        string   `json:"title"`
+	StartMonth   Month    `json:"startMonth"`
+	EndMonth     *Month   `json:"endMonth"`
+	Description  []string `json:"description"`
+	Technologies []string `json:"technologies"`
 }
