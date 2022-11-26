177cf5a49e85d2dcd9501761b79462654b790d13
meriy100 <ttattataa@gmail.com>
2022-11-26 20:27:57 +0900

    [Fix] rename

--- usecase/skillInteractor.go
+++ usecase/skillInteractor.go
@@ -8,7 +8,7 @@ import (
 type SkillInteractor struct {
 	outputPort      ports.SkillOutputPort
 	postRepository  ports.PostRepository
-	SkillRepository ports.SkillRepository
+	skillRepository ports.SkillRepository
 }
 
 func NewSkillInteractor(outputPort ports.SkillOutputPort, postRepository ports.PostRepository, skillRepository ports.SkillRepository) ports.SkillInputPort {
@@ -31,7 +31,7 @@ func (s *SkillInteractor) UpdateSkills() error {
 	}
 
 	for _, skill := range skills {
-		err := s.SkillRepository.Save(skill)
+		err := s.skillRepository.Save(skill)
 		if err != nil {
 			return s.outputPort.OutputSkillSaveError(skill, err)
 		}
@@ -41,7 +41,7 @@ func (s *SkillInteractor) UpdateSkills() error {
 }
 
 func (s *SkillInteractor) IndexSkills() error {
-	skills, err := s.SkillRepository.All()
+	skills, err := s.skillRepository.All()
 	if err != nil {
 		return s.outputPort.OutputFetchSkillsError(err)
 	}
