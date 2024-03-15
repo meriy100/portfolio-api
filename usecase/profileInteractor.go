4d2e3d415bba3462a077f1328d690d3d29c374a8
meriy100 <ttattataa@gmail.com>
2024-03-15 23:20:26 +0900

    refact(usecases): esa post id を定数化 / 環境変数 に移す予定

--- usecase/profileInteractor.go
+++ usecase/profileInteractor.go
@@ -21,6 +21,9 @@ func NewProfileInteractor(outputPort ports.ProfileOutputPort, postRepository por
 		contentDeliveryRepository,
 	}
 }
+
+const ProfilePostID = 253
+
 func (pi *ProfileInteractor) ShowProfile() error {
 	profile, err := pi.profileRepository.Find()
 	if err != nil {
@@ -31,7 +34,7 @@ func (pi *ProfileInteractor) ShowProfile() error {
 }
 
 func (pi *ProfileInteractor) UpdateProfile() error {
-	post, err := pi.postRepository.FetchPost(253)
+	post, err := pi.postRepository.FetchPost(ProfilePostID)
 	if err != nil {
 		return pi.outputPort.OutputFetchPostError(err)
 	}
