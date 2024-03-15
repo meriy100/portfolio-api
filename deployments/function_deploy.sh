ef86ffec45197235e014581735d580a2aa038bba
meriy100 <ttattataa@gmail.com>
2024-03-15 21:57:31 +0900

    cd: cd.yml と deploy.yml を用意 / ただし Skills のみ

--- deployments/function_deploy.sh
+++ deployments/function_deploy.sh
@@ -9,6 +9,7 @@ gcloud functions deploy $FUNCTION_NAME \
   --entry-point=$ENTRY_POINT \
   --trigger-http \
   --set-secrets=ESA_ACCESS_TOKEN=ESA_ACCESS_TOKEN:latest,VERCEL_DEPLOY_HOOK_URL=VERCEL_DEPLOY_HOOK_URL:latest \
+  --service-account=$SERVICE_ACCOUNT \
   --max-instances=$MAX_INSTANCES \
   --concurrency=$CONCURRENCY \
   --timeout=$TIMEOUT
