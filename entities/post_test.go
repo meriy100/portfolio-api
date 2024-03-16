2d657a321aabc0db456f5ebfe12f48111e9deccb
meriy100 <ttattataa@gmail.com>
2024-03-16 14:22:04 +0900

    test(entities): TestPostToHistories の文量を削減

--- entities/post_test.go
+++ entities/post_test.go
@@ -1,23 +1,22 @@
 package entities
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestPost_ToHistories(t *testing.T) {
-	tests := []struct {
-		name   string
+	tests := map[string]struct {
 		post   Post
-		got    []History
+		got    []*History
 		gotErr bool
 	}{
-		{
-			"success",
+		"success": {
 			Post{
 				BodyMd: "# 株式会社 Photosynth.inc\r\n## スマートロック関連のプロダクト開発業務\r\n### startMonth\r\n2020/03\r\n### endMonth\r\n\r\n### description\r\n- サーバーサイド設計, 開発, レビュー\r\n- フロントエンド設計, 開発, レビュー\r\n\r\n###  technologyUsed \r\n- Ruby on Rails\r\n- Vue.js\r\n- HIDDEN\r\n\r\n# 合同会社フレイズ (業務委託)\r\n\r\n## スマートフォンブラウザでの映像撮影, 画像認識処理\r\n### startMonth\r\n 2019/11\r\n###  endMonth\r\n 2020/02\r\n###    description\r\n- 技術調査 (Web カメラ, 顔認識, 文字認識等)\r\n- フロントエンド設計, 開発\r\n###  technologyUsed \r\n- TypeScript\r\n- Google Cloud Platform\r\n- Vue.js\r\n- OpenCV\r\n- face api (tensorflow.js)\r\n\r\n## サンプルプロダクト\r\n### startMonth\r\n 2019/11\r\n###  endMonth\r\n 2020/02\r\n###    description\r\n- 技術調査 (Web カメラ, 顔認識, 文字認識等)\r\n- フロントエンド設計, 開発\r\n###  technologyUsed \r\n- TypeScript\r\n- Google Cloud Platform\r\n- Vue.js\r\n- OpenCV\r\n- face api (tensorflow.js)\r\n",
 			},
-			[]History{
+			[]*History{
 				{
 					"株式会社 Photosynth.inc",
 					[]Product{
@@ -79,51 +78,15 @@ func TestPost_ToHistories(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
 			histories, err := tt.post.ToHistories()
 			if (err != nil) != tt.gotErr {
-				t.Errorf("ToHistory() : err = %v want gotErr %v\n", err, tt.gotErr)
-			}
-			if len(histories) != len(tt.got) {
-				t.Errorf("length different : histories = %v want %v\n", histories, tt.got)
+				t.Fatalf("ToHistory() : err = %v want gotErr %v\n", err, tt.gotErr)
 			}
 
-			for i, got := range tt.got {
-				h := histories[i]
-				if histories[i].Organization != got.Organization {
-					t.Errorf("histories.[%d].Organization = %v want %v\n", i, h.Organization, got.Organization)
-
-				}
-
-				if len(h.Products) != len(got.Products) {
-					t.Errorf("histories.[%d].Products length different : Products = %v want %v\n", i, h.Products, got.Products)
-				}
-
-				for j, gotP := range got.Products {
-					p := h.Products[j]
-					if p.Title != gotP.Title {
-						t.Errorf("histories.[%d].Products[%d].Title = %v want %v\n", i, j, p.Title, gotP.Title)
-					}
-					if p.StartMonth != gotP.StartMonth {
-						t.Errorf("histories.[%d].Products[%d].StartMonth= %v want %v\n", i, j, p.StartMonth, gotP.StartMonth)
-					}
-					if (p.EndMonth == nil) != (gotP.EndMonth == nil) {
-						t.Errorf("histories.[%d].Products[%d].EndMonth= %v want %v\n", i, j, p.EndMonth, gotP.EndMonth)
-					} else if p.EndMonth != nil && gotP.EndMonth != nil {
-						if *p.EndMonth != *gotP.EndMonth {
-							t.Errorf("histories.[%d].Products[%d].EndMonth= %v want %v\n", i, j, p.EndMonth, gotP.EndMonth)
-						}
-					}
-
-					if !reflect.DeepEqual(p.Description, gotP.Description) {
-						t.Errorf("histories.[%d].Products[%d].Description= %s want %s\n", i, j, p.Description, gotP.Description)
-					}
-
-					if !reflect.DeepEqual(p.Technologies, gotP.Technologies) {
-						t.Errorf("histories.[%d].Products[%d].Technologies= %v want %v\n", i, j, p.Technologies, gotP.Technologies)
-					}
-				}
+			if diff := cmp.Diff(tt.got, histories); diff != "" {
+				t.Errorf("ToHistory() mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
